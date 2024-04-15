44826ad75dda3b3a2480b14db60769c34e8adc18
minutemancarlo <67291699+minutemancarlo@users.noreply.github.com>
2024-04-16 02:52:25 +0800

    Added Dashboard for Cashier

---
+++ Server/Controllers/DashboardController.cs
@@ -0,0 +1,66 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SharedLibrary;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DashboardManagerController : ControllerBase
+    {
+        private readonly string _connectionString;
+
+        public DashboardManagerController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        
+
+        [HttpGet("getcashierdashboardall")]
+        public async Task<ActionResult<IEnumerable<CashierDashboardDisplay>>> GetCashierDashboard1(string emailaddress)
+        {
+            try
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("@Chart", 0);
+                parameters.Add("@Email", emailaddress);
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    var results = await connection.QueryAsync<CashierDashboardDisplay>("GetCashierChart", parameters, commandType: CommandType.StoredProcedure);
+                    return Ok(results.ToList());
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Exception Occured: {ex.Message}");
+            }
+        }
+
+        [HttpGet("getcashierdashboardall2")]
+        public async Task<ActionResult<IEnumerable<CashierDashboardDisplay>>> GetCashierDashboard2(string emailaddress)
+        {
+            try
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("@Chart", 1);
+                parameters.Add("@Email", emailaddress);
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    var results = await connection.QueryAsync<CashierDashboardDisplay>("GetCashierChart", parameters, commandType: CommandType.StoredProcedure);
+                    return Ok(results.ToList());
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Exception Occured: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ SharedLibrary/CashierDashboardDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary
+{
+    public class CashierDashboardDisplay
+    {
+        public double SundayTotal { get; set; }
+        public double MondayTotal { get; set; }
+        public double TuesdayTotal { get; set; }
+        public double WednesdayTotal { get; set; }
+        public double ThursdayTotal { get; set; }
+        public double FridayTotal { get; set; }
+        public double SaturdayTotal { get; set; }
+    }
+}
